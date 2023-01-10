ae1f29457afa7c033c7938da9d6f3a3c20ae2eb5
matthieubrunetdebaines <matbrudeb@gmail.com>
2023-01-10 14:15:04 +0100

    version prête à être présentée

--- app3.js
+++ app3.js
@@ -1,12 +1,24 @@
+const menuHamburger = document.querySelector(".menu_burger");
+const navLinks = document.querySelector(".nav_links");
+
+menuHamburger.addEventListener('click',()=>{
+    navLinks.classList.toggle("menu_mobile")
+})
+
+
 
 function afficher() {
     event.preventDefault();
+    const fermerLaCarte = document.querySelector(".icon_takeoff");
     const laCarte = document.querySelector(".iframe_map");
     const afficherLaCarte = document.querySelector(".show_map");
     laCarte.setAttribute("style","visibility:visible");
     afficherLaCarte.setAttribute("style","visibility:hidden");
+    fermerLaCarte.setAttribute("style","visibility:visible");
 }
 
+
+// Au clic sur le bouton de fermeture => la iframe et le bouton disparaisse => afficher apparait
 function decollage(){
     event.preventDefault();
     const fermerLaCarte = document.querySelector(".icon_takeoff");
@@ -15,4 +27,21 @@ function decollage(){
     fermerLaCarte.setAttribute("style","margin-top:-100%");
     laCarte.setAttribute("style","visibility:hidden");
     afficherLaCarte.setAttribute("style","visibility:visible");
-}
+    alert("Bon vol voyageur :)");
+} 
+
+// function verifDecollage(){
+//     event.preventDefault();
+//     const fermerLaCarte = document.querySelector(".icon_takeoff");
+//     const laCarte = document.querySelector(".iframe_map");
+//     const afficherLaCarte = document.querySelector(".show_map");
+//     if(laCarte.hasAttributes("style","visibility:hidden") && afficherLaCarte.hasAttributes("style","visibility:visible")) {
+//         fermerLaCarte.setAttribute("style","visibility:visible");
+//     } else {
+//         fermerLaCarte.setAttribute("style","visibility:hidden");
+//     }
+// }
+
+// function doubleDecollage(){
+//     decollage() + verifDecollage();
+// }
